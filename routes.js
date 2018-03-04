25085261228bc58f8783fa1c53881f33ac8d2aee
obevanken <vanen11@bk.ru>
2018-03-04 21:31:57 +0700

    authenticate

--- routes.js
+++ routes.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/auth');
+}
+
+var isAuth = function (req, res, next){
+  if (req.isAuthenticated()){
+    res.redirect('/');
+  }
+  return next();
+}
+
 module.exports = function (passport) {
-  router.get("/register", (req, res) => {
+
+  router.get("/register", isAuth, (req, res) => {
     res.render("register", {
       messages: req.flash('message')
     })
+
   })
 
   router.post("/register", passport.authenticate('register', { successRedirect: '/auth',
                                      failureRedirect: '/register',
                                      failureFlash: true }));
 
-  router.get("/auth", (req, res) => {
+  router.get("/auth",isAuth, (req, res) => {
     res.render("auth", {
       messages: req.flash('message')
     });
@@ -23,23 +38,23 @@ module.exports = function (passport) {
                                        failureFlash: true }));
 
 
-  router.get("/", (req, res) => {
+  router.get("/", isAuthenticated, (req, res) => {
     res.sendStatus("404");
   })
 
-  router.get("/post/:id", (req, res) => {
+  router.get("/post/:id", isAuthenticated, (req, res) => {
     res.sendStatus(404);
   })
 
-  router.get("/post/new", (req, res) => {
+  router.get("/post/new", isAuthenticated, (req, res) => {
     res.sendStatus(404);
   })
 
-  router.post("/post/new", (req, res) => {
+  router.post("/post/new", isAuthenticated, (req, res) => {
     res.sendStatus(404);
   })
 
-  router.get("/author/:id", (req, res) => {
+  router.get("/author/:id", isAuthenticated, (req, res) => {
     res.sendStatus(404);
   })
 
